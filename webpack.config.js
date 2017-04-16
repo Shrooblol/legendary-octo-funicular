307201b766e3c5afac36d1dc85ed95976929990b
Joshua <joshkap2015@gmail.com>
2017-04-17 00:19:05 +0200

    added hot replacement for react

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var path = require("path");
+
+const webpack = require('webpack');
+const { resolve } = require('path');
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, "dist"), // resolving the output location for which app.js will be converted to.
-        filename: 'app.bundle.js' // this is the filename of the converted app.js
-    },
-    module: {
-        rules: [
+  context: resolve(__dirname, 'src'),
+    entry: [
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './index.js'
+    // the entry point of our app
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    // the output bundle
+
+    path: resolve(__dirname, 'dist'),
+
+    publicPath: '/'
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+            {
+                test: /\.jsx?$/,
+                use: [ 'babel-loader', ],
+                exclude: /node_modules/
+            },
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
@@ -18,19 +49,24 @@ module.exports = {
                     publicPath: '/dist'
                 })
             },
-            {
+           {
               test: /\.js$/,
               exclude: /node_modules/,
               use: 'babel-loader'
             }
         ]
-    },
+            },
+
+
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
+        contentBase: resolve(__dirname, "dist"),
         compress: true,
         stats: "errors-only",
-        open: true
+        open: true,
+        hot: true,
+        publicPath: '/'
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Project Demo',
@@ -38,12 +74,17 @@ module.exports = {
             //     collapseWhitespace: true
             // },
             hash: true,
-            template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+            template: 'index.html', // Load a custom template (ejs by default see the FAQ for details)
         }),
         new ExtractTextPlugin({
             filename: 'app.css',
             disable: false,
             allChunks: true
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        // enable HMR globally
+
+        new webpack.NamedModulesPlugin()
+        // prints more readable module names in the browser console on HMR updates
     ]
 }
